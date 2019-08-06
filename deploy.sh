185d1386352eb823ec1655a227bcc7e7610b4128
Eduardo Tenório <embatbr@gmail.com>
2019-08-06 15:55:26 -0300

    Generic deploy.

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,19 @@ export PROJECT_ROOT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd ${PROJECT_ROOT_PATH}
 
 
-rm -Rf ${PROJECT_ROOT_PATH}/../dotzpay/composer/dags/debussy
-cp -R debussy ${PROJECT_ROOT_PATH}/../dotzpay/composer/dags
+TARGET_PROJECT="$1"
+if [ "${TARGET_PROJECT}" == "dotzpay" ]
+then
+    TARGET_PATH="${TARGET_PROJECT}/composer"
+elif [ "${TARGET_PROJECT}" == "dotz-workflows" ]
+then
+    TARGET_PATH="${TARGET_PROJECT}"
+fi
+
+
+rm -Rf ${PROJECT_ROOT_PATH}/../${TARGET_PATH}/dags/debussy
+cp -R debussy ${PROJECT_ROOT_PATH}/../${TARGET_PATH}/dags
 
 
 # python setup.py bdist_egg
-# cp dist/debussy-1.0.0-py3.6.egg ../dotzpay/composer/dags/debussy.egg
+# cp dist/debussy-1.0.0-py3.6.egg ../${TARGET_PATH}/dags/debussy.egg
